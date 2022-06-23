c842c9b8d6fb2cd810883285d838d56dd63b1362
Taras Makarchuk <2278452@gmail.com>
2022-06-23 19:09:44 +0300

    add folder navigation

--- client/src/components/disk/filesList/file/File.jsx
+++ client/src/components/disk/filesList/file/File.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 import './file.css';
 import folderImg from '../../../../assets/img/folder.svg';
 import fileImg from '../../../../assets/img/file.svg';
+import { useDispatch, useSelector } from "react-redux";
+import { pushToStack, setCurrentDir } from "../../../../reducers/fileReducer";
 
 const File = ({ file }) => {
   const date = new Date(file.date).toLocaleString();
+  const dispatch = useDispatch();
+  const currentDir = useSelector(state => state.files.currentDir);
+
+  function openFolderHandler() {
+    dispatch(pushToStack(currentDir));
+    dispatch(setCurrentDir(file._id));
+  }
+
   return (
-    <div className='file'>
+    <div
+      className='file'
+      onClick={ file.type === 'dir' ? () => openFolderHandler(): ''}
+    >
       <img src={file.type === 'dir' ? folderImg : fileImg} alt="image" className="file__img"/>
       <div className="file__title">{file.name}</div>
       <div className="file__date">{date}</div>
