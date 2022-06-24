731d16cf38b4ffd1698cfea65793af3ef2c51c19
Taras Makarchuk <2278452@gmail.com>
2022-06-24 15:20:07 +0300

    add files upload

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 const config = require('config');
 const authRouter = require('./routes/auth.routes');
 const fileRouter = require('./routes/file.routes');
+const fileUpload = require('express-fileupload');
 const corsMiddleware = require('./middleware/cors.middleware');
 
 const app = express();
 const HOST = config.get('host');
 const PORT = config.get('serverPort') || 8008;
 
+app.use(fileUpload({}));
 app.use(corsMiddleware);
 app.use(express.json());
 app.use('/api/auth', authRouter);
